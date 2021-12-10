b1b3e5818cfb8df1f6ab7d2545cf3cc57d17b915
Gabriel DENAJAR <gabriel.denajar@etu.unilim.fr>
2021-12-10 11:47:01 +0000

    Add cors module and fix a typo for getUser

--- controllers/usersCtrl.js
+++ controllers/usersCtrl.js
@@ -18,7 +18,7 @@ module.exports = {
     getUser: function (req, res) {
         models.User.findByPk(req.params.id
         ).then(function (user) {
-            res.status(200).json(user.id)
+            res.status(200).json(user)
         })
     },
     updateUser: function (req, res) {
--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT;
 const apiRouter = require('./routes/apiRouter').router;
+const cors = require('cors');
+
+app.use(cors());
 
 // middleware
 app.use(bodyParser.json()) // for parsing application/json
